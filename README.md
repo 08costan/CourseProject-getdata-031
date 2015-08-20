be7175d673f59506d86d49771b06e01d4153072f
Vincent Costanza <costanza.vincent@gmail.com>
2015-08-20 15:38:00 +0200

    Files for Course Project

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# CourseProject-getdata-031
-Repository for my submission to the course project for Coursera's "Getting and Cleaning Data" MOOC
+# README.md
+
+## General information
+This repository contains my personal work for "Getting and Cleaning Data" course project on Coursera.
+
+The goal of this study is to collect a dataset from UCI Machine Learning Repository, and build a tidy summary dataset following the
+tidy data principles (see Hadley Wickham's publication on tidy data: http://vita.had.co.nz/papers/tidy-data.pdf)
+
+The input is a collection of data on ~500 variables measured from the accelerometers of a Samsung Galaxy S smartphone.
+A group of ~30 people was asked to perform different activities (laying, walking, standing, running, ...), 30-90 observations were recorded for each pair of subject/activity.
+
+The output of this work is a text file 'tidyData.txt' containing the average value of each variable labelled 'std()' or 'mean()' for each pair of subject/activity.
+
+## This repo includes the following files:
+
+* This README.md file
+* The code book, describing how my R code is working, how the tidy dataset is organized (variables and rows)
+* run_analysis.R: the R source file
+* tidyData.txt : a copy of the output from the R sourcefile
---
+++ run_analysis.R
@@ -0,0 +1,58 @@
+# Coursera - Getting and Cleaning Data
+# Course Project (Week 3)
+
+
+# PART 0 - DOWNLOADING AND UNZIPPING DATA
+
+if(!file.exists("data.zip")) {
+    fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+    download.file(fileUrl,destfile = "data.zip",method="curl")
+}
+unzip("data.zip")
+
+## PART 1 - READING RAW DATA
+
+activity_labels<-read.csv("UCI HAR Dataset/activity_labels.txt",header=FALSE,sep=" ")
+features<-read.csv("UCI HAR Dataset/features.txt",header=FALSE,sep=" ")
+
+subject_train<-read.csv("UCI HAR Dataset/train/subject_train.txt",header=FALSE)
+X_train<-read.table("UCI HAR Dataset/train/X_train.txt")
+Y_train<-read.csv("UCI HAR Dataset/train/y_train.txt",header=FALSE)
+
+subject_test<-read.csv("UCI HAR Dataset/test/subject_test.txt",header=FALSE)
+X_test<-read.table("UCI HAR Dataset/test/X_test.txt")
+Y_test<-read.csv("UCI HAR Dataset/test/y_test.txt",header=FALSE)
+
+## PART 2 - PROCESSING 'TRAIN' DATA (X_train, Y_train, subject_train)
+
+colnames(X_train)<-features$V2                          # Adding variable names from "features.txt"
+columnSelect<-grep("mean\\(|std\\(",colnames(X_train))  # We only keep variables containing "mean()" or "std()" in their names
+X_train<-X_train[,columnSelect]                     
+X_train=cbind(X_train,Y_train,subject_train)            # Merging data (X_train, Y_train, subject_train)
+colnames(X_train)<-c(colnames(X_train)[1:66],"activity","subject")
+
+## PART 3 - PROCESSING 'TEST' DATA (X_test, Y_test, subject_test)
+
+colnames(X_test)<-features$V2                          # Adding variable names from "features.txt"
+columnSelect<-grep("mean\\(|std\\(",colnames(X_test))  # We only keep variables containing "mean()" or "std()" in their names
+X_test<-X_test[,columnSelect]                     
+X_test=cbind(X_test,Y_test,subject_test)                # Merging data (X_test, Y_test, subject_test)
+colnames(X_test)<-c(colnames(X_test)[1:66],"activity","subject")
+
+## PART 4 - MERGING 'TEST' AND 'TRAIN' DATASETS
+
+data<-rbind(X_train,X_test)
+data$activity_label<-activity_labels[data$activity,2]   # Adding explicit activity labels to our dataset
+data<-data[order(data$subject,data$activity),]          # Sorting data by subject and activity
+
+## PART 5 - CREATING A SECOND TIDY DATASET
+## This part uses melt/cast functions from library reshape2 to create a tidy data set containing:
+## the average of each variable, for each subject and each activity.
+
+library(reshape2)
+dataMelt <- melt(data,id=c("activity_label","subject","activity_label"))
+tidyData <- dcast(dataMelt, subject+activity_label~variable,mean)
+
+## PART 6 - SAVING THE TIDY DATASET TO A TXT FILE
+
+write.table(tidyData,file="tidyData.txt",row.names=FALSE)
